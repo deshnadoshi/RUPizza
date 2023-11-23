898e211b07e5798f911218f735ff410f02f2a5f0
Emboar02 <pikaemboar202@gmail.com>
2023-11-23 12:34:35 -0500

    Attempted to code createPizza, hopefully it works like this. Changed constructor for Pizza.

--- src/main/java/com/example/rupizza/PizzaMaker.java
+++ src/main/java/com/example/rupizza/PizzaMaker.java
@@ -4,6 +4,22 @@ package com.example.rupizza;
 // CAN ONLY HAVE createPizza()!!!
 public class PizzaMaker {
     public static Pizza createPizza(String pizzaType) {
-        return null; // placeholder
+        Pizza pizza = null;
+        if (pizzaType.equals("Deluxe")) {
+            pizza = new Deluxe(Size.SMALL, null, false, false);
+        } else if (pizzaType.equals("Meatzza")) {
+            pizza = new Meatzza(Size.SMALL, null, false, false);
+        } else if (pizzaType.equals("Pepperoni")) {
+            pizza = new Pepperoni(Size.SMALL, null, false, false);
+        } else if (pizzaType.equals("Seafood")) {
+            pizza = new Seafood(Size.SMALL, null, false, false);
+        } else if (pizzaType.equals("Supreme")) {
+            pizza = new Supreme(Size.SMALL, null, false, false);
+        } else if (pizzaType.equals("BuildYourOwn")) {
+            pizza = new BuildYourOwn(Size.SMALL, null, false, false, null);
+        } else {
+            return null;
+        }
+        return pizza; // placeholder
     }
 }
