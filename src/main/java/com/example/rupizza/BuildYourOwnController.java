0ef24ef5743b1f31df5b7185a79dcaad62193a92
Emboar02 <45805212+Emboar02@users.noreply.github.com>
2023-11-23 00:32:39 -0500

    Revert "Main Menu Functionality"

--- src/main/java/com/example/rupizza/BuildYourOwnController.java
+++ src/main/java/com/example/rupizza/BuildYourOwnController.java
@@ -3,6 +3,4 @@ package com.example.rupizza;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 public class BuildYourOwnController {
-
-
 }
--- src/main/java/com/example/rupizza/SpecialtyPizzasController.java
+++ src/main/java/com/example/rupizza/SpecialtyPizzasController.java
@@ -1,25 +1,6 @@
 package com.example.rupizza;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.TilePane;
-
 public class SpecialtyPizzasController {
-    @FXML
-    private ComboBox pizza_options;
-    @FXML
-    private void initialize() {
-        ObservableList<String> items =
-                FXCollections.observableArrayList("Deluxe", "Supreme", "Meatzza", "Seafood", "Pepperoni");
-        pizza_options.setItems(items);
-        pizza_options.getSelectionModel().select(0);
-
-        //comboBox.getItems().removeAll(comboBox.getItems());
-        //comboBox.getItems().addAll("Deluxe", "Supreme", "Meatzza", "Seafood", "Pepperoni");
-        //comboBox.getSelectionModel().select("Deluxe");
-    }
 }
