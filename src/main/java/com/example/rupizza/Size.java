1569d4b43126069500c96ab02ffe77dc852349cc
deshnadoshi <deshna.doshi@gmail.com>
2023-11-27 13:50:20 -0500

    Size comments

--- src/main/java/com/example/rupizza/Size.java
+++ src/main/java/com/example/rupizza/Size.java
@@ -1,5 +1,9 @@
 package com.example.rupizza;
 
+/**
+ * Enum class to represent the Size.
+ * @author Haejin Song, Deshna Doshi
+ */
 public enum Size {
     SMALL(0),
     MEDIUM(2),
@@ -7,10 +11,18 @@ public enum Size {
 
     private final int CODE;
 
+    /**
+     * Constructor for the Size enum.
+     * @param code Integer corresponding to the size.
+     */
     Size(int code) {
         this.CODE = code;
     }
 
+    /**
+     * Provides the code corresponding to the size.
+     * @return Code (integer) of the size.
+     */
     public int getCode() {
         return CODE;
     }
